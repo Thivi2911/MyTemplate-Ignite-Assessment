362193497b349bdf469af5a8fbd8838f4619f9e6
Thivi29 <thivideepak2911@gmail.com>
2026-08-19 21:55:13 +0530

    Fix test database configuration for Windows

--- appname/settings.py
+++ appname/settings.py
@@ -1,5 +1,5 @@
 import os
-import tempfile
+
 
 class Config(object):
     # run flask generate_secret_key
@@ -80,8 +80,7 @@ class TestConfig(Config):
     DEBUG = True
     DEBUG_TB_INTERCEPT_REDIRECTS = False
 
-    db_file = tempfile.NamedTemporaryFile()
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + db_file.name
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
     SQLALCHEMY_ECHO = False  # Optionally enable if you want to see database actions
     ASSETS_DEBUG = True
 
